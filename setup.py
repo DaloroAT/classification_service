9f430176c72fe563367ffa3ad3f21d0dc6b33d10
Алексей Тарасов <aleksei.v.tarasov@gmail.com>
2020-03-23 10:06:16 +0300

    added requirements and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="classification_service",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Service for image classification with pytorch, flask and redis",
+    author="Aleksei Tarasov",
+    license=""
+)
